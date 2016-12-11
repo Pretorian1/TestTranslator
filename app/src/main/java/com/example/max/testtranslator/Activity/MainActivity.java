f6735760fd92987aa07325149043c15af480b51f
Maksym Koval <pretorian1@i.ua>
2016-12-11 13:43:34 +0200

    Fourth working commit (added offline dictionary not working for now)

--- app/src/main/java/com/example/max/testtranslator/Activity/MainActivity.java
+++ app/src/main/java/com/example/max/testtranslator/Activity/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.max.testtranslator.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -20,7 +23,10 @@ import com.google.gson.Gson;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -38,21 +44,81 @@ public class MainActivity extends AppCompatActivity {
     private Button mButton;
     private Spinner mSpinnerFrom;
     private Spinner mSpinnerTo;
+    private Button mButtonParser;
+    final String LOG_TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mInputText = (EditText) findViewById(R.id.input_text);
+        mInputText.setImeOptions(EditorInfo.IME_ACTION_DONE);
         mOutputText = (EditText) findViewById(R.id.output_text);
+        mOutputText.setImeOptions(EditorInfo.IME_ACTION_DONE);
         mButton =(Button)findViewById(R.id.button);
         mSpinnerFrom =(Spinner) findViewById(R.id.spinnerFrom);
         mSpinnerTo = (Spinner) findViewById(R.id.spinnerTo);
+        mButtonParser = (Button) findViewById(R.id.buttonParser);
         EventBus.getDefault().register(this);
         mButton.setOnClickListener(new View.OnClickListener() {//
             @Override
             public void onClick(View v) {
               translateText();
+            }
+        });
+        mButtonParser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tmp = "";
+
+                try {
+                    XmlPullParser xpp = prepareXpp();
+
+                    while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {
+                        switch (xpp.getEventType()) {
+                            // начало документа
+                            case XmlPullParser.START_DOCUMENT:
+                                Log.d(LOG_TAG, "START_DOCUMENT");
+                                break;
+                            // начало тэга
+                            case XmlPullParser.START_TAG:
+                                Log.d(LOG_TAG, "START_TAG: name = " + xpp.getName()
+                                        + ", depth = " + xpp.getDepth() + ", attrCount = "
+                                        + xpp.getAttributeCount());
+                                tmp = "";
+                                for (int i = 0; i < xpp.getAttributeCount(); i++) {
+                                    tmp = tmp + xpp.getAttributeName(i) + " = "
+                                            + xpp.getAttributeValue(i) + ", ";
+                                }
+                                if (!TextUtils.isEmpty(tmp))
+                                    Log.d(LOG_TAG, "Attributes: " + tmp);
+                                break;
+                            // конец тэга
+                            case XmlPullParser.END_TAG:
+                                Log.d(LOG_TAG, "END_TAG: name = " + xpp.getName());
+                                break;
+                            // содержимое тэга
+                            case XmlPullParser.TEXT:
+                                Log.d(LOG_TAG, "text = " + xpp.getText());
+                                break;
+
+                            default:
+                                break;
+                        }
+                        // следующий элемент
+                        xpp.next();
+                    }
+                    Log.d(LOG_TAG, "END_DOCUMENT");
+
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            XmlPullParser prepareXpp() {
+                return getResources().getXml(R.xml.english_russian);
             }
         });
     }
